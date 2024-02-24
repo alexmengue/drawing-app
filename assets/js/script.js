1d65294013b5f8e09954fbbd7ec74656ab13e384
Alex <alexmengue1990@hotmail.com>
2024-02-24 01:35:43 -0300

    added drawing function

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector('canvas');
+ctx = canvas.getContext('2d');
+
+const drawing = (event) => {
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+};
+
+canvas.addEventListener('mousemove', drawing);
