597edfe3807014a99c2dd76a60fbd686cc334892
Alex <alexmengue1990@hotmail.com>
2024-02-26 23:48:59 -0300

    att

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const canvas = document.querySelector('canvas');
 ctx = canvas.getContext('2d');
 
+window.addEventListener('load', () => {
+  //soon to be
+});
+
 const drawing = (event) => {
   ctx.lineTo(event.offsetX, event.offsetY);
   ctx.stroke();
