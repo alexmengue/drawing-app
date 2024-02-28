9c7634ca94fb4d792741b04bcd53d0f2e8f1d468
Alex <alexmengue1990@hotmail.com>
2024-02-28 00:01:41 -0300

    att

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
 const canvas = document.querySelector('canvas');
 ctx = canvas.getContext('2d');
 
+let isDrawing = false;
+
 window.addEventListener('load', () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 });
 
+const startDraw = () => {
+  //soon
+};
+
 const drawing = (event) => {
+  if (!isDrawing) return;
   ctx.lineTo(event.offsetX, event.offsetY);
   ctx.stroke();
 };
 
+canvas.addEventListener('mousedown', startDraw);
 canvas.addEventListener('mousemove', drawing);
-
-//soon to be
