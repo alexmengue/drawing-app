a230d8ec0a40408548c84170ac8e192b31a7940f
Alex <alexmengue1990@hotmail.com>
2024-03-02 11:09:12 -0300

    att brush tool

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const canvas = document.querySelector('canvas');
 ctx = canvas.getContext('2d');
 
 let isDrawing = false;
+let brushWidth = 5;
 
 window.addEventListener('load', () => {
   canvas.width = canvas.offsetWidth;
@@ -11,6 +12,7 @@ window.addEventListener('load', () => {
 const startDraw = () => {
   isDrawing = true;
   ctx.beginPath();
+  ctx.lineWidth = brushWidth;
 };
 
 const drawing = (event) => {
