f9663ac286ab5f8107df0db367b62a8ff9a37c56
Alex <alexmengue1990@hotmail.com>
2024-03-06 13:01:45 -0300

    added draw rect function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,45 @@
-const canvas = document.querySelector('canvas');
-const toolBtns = document.querySelectorAll('.tool');
+const canvas = document.querySelector('canvas'),
+toolBtns = document.querySelectorAll('.tool'),
 ctx = canvas.getContext('2d');
 
-let isDrawing = false;
-let selectedTool = 'brush';
-let brushWidth = 5;
+let prevMouseX, prevMouseY,
+isDrawing = false,
+selectedTool = 'brush',
+brushWidth = 5;
 
 window.addEventListener('load', () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 });
 
-const startDraw = () => {
+const drawRect = (event) => {
+  ctx.strokeRect(event.offsetX, event.offsetY, prevMouseX - event.offsetX, prevMouseY - event.offsetY);
+}
+
+const startDraw = (event) => {
   isDrawing = true;
+  prevMouseX = event.offsetX;
+  prevMouseY = event.offsetY;
   ctx.beginPath();
   ctx.lineWidth = brushWidth;
 };
 
 const drawing = (event) => {
   if (!isDrawing) return;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+
+  if (selectedTool === 'brush') {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+  } else if (selectedTool === 'rectangle') {
+    drawRect(event);
+  }
 };
 
 toolBtns.forEach(btn => {
   btn.addEventListener('click', () => {
     document.querySelector('.options .active').classList.remove('active');
-    selectedTool = btn.id;
     btn.classList.add('active');
+    selectedTool = btn.id;
   });
 });
 
