0d686b099ba0943afd2f9bf2a86714801edc961c
Alex <alexmengue1990@hotmail.com>
2024-03-11 01:33:32 -0300

    att draw circle feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ const drawCircle = (event) => {
   ctx.beginPath();
   let radius = Math.sqrt(Math.pow((prevMouseX - event.offsetX), 2) + Math.pow((prevMouseY - event.offsetY), 2));
   ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
-  ctx.stroke();
+  fillColor.checked ? ctx.fill() : ctx.stroke();
 };
 
 const startDraw = (event) => {
