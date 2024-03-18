00035b696e9cdf917492b9094ebe8b92f6db181a
Alex <alexmengue1990@hotmail.com>
2024-03-18 12:32:55 -0300

    att color tool

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const canvas = document.querySelector('canvas'),
 toolBtns = document.querySelectorAll('.tool'),
 fillColor = document.querySelector('#fill-color'),
 sizeSlider = document.querySelector('#size-slider'),
+colorBtns = document.querySelector('.colors .option'),
 ctx = canvas.getContext('2d');
 
 let prevMouseX, prevMouseY, snapshot,
@@ -72,6 +73,10 @@ toolBtns.forEach(btn => {
 });
 
 sizeSlider.addEventListener('change', () => brushWidth = sizeSlider.value);
+
+colorBtns.forEach(btn => {
+  //soon
+});
 
 canvas.addEventListener('mousedown', startDraw);
 canvas.addEventListener('mousemove', drawing);
